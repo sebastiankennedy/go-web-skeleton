405be458741ac8b80ce73541604dbc6fe4c4716d
SebastianKennedy <sebastiankennedy@foxmail.com>
2021-11-02 00:11:47 +0800

    Added adminlte html template.

--- app/http/controllers/admin_controller.go
+++ app/http/controllers/admin_controller.go
@@ -1,11 +1,8 @@
 package controllers
 
 import (
-	"github.com/sebastiankennedy/go-web-skeleton/pkg/logger"
 	"github.com/sebastiankennedy/go-web-skeleton/pkg/mvc"
-	"html/template"
 	"net/http"
-	"path/filepath"
 )
 
 type AdminController struct {
@@ -13,22 +10,5 @@ type AdminController struct {
 }
 
 func (*AdminController) Index(w http.ResponseWriter, r *http.Request) {
-	// 设置模板相对路径
-	viewDir := "resources/views"
-
-	// 所有布局模板文件 Slice
-	files, err := filepath.Glob(viewDir + "/admin/layouts/*.gohtml")
-
-	// 在 Slice 里新增我们的目标文件
-	newFiles := append(files, viewDir+"/admin/dashboard/index.gohtml")
-
-	// 解析模板文件
-	tmpl, err := template.ParseFiles(newFiles...)
-	logger.Error(err)
-
-	// 渲染模板，将数据传输进去
-	err = tmpl.ExecuteTemplate(w, "app", nil)
-	if err != nil {
-		logger.Error(err)
-	}
+	mvc.RenderAdmin(w, nil, "dashboard.index")
 }
--- pkg/mvc/view.go
+++ pkg/mvc/view.go
@@ -1,4 +1,61 @@
 package mvc
 
-type View struct {
+import (
+	"github.com/sebastiankennedy/go-web-skeleton/pkg/logger"
+	"github.com/sebastiankennedy/go-web-skeleton/pkg/router"
+	"html/template"
+	"io"
+	"path/filepath"
+	"strings"
+)
+
+type Data map[string]interface{}
+
+func RenderAdmin(w io.Writer, data Data, tplFiles ...string) {
+	RenderTemplate(w, "app", data, tplFiles...)
+}
+
+func RenderTemplate(w io.Writer, name string, data Data, tplFiles ...string) {
+	var err error
+
+	// 生成模板文件
+	allFiles := getAdminTemplateFiles(tplFiles...)
+
+	// 解析模板文件
+	tmpl, err := template.New("").Funcs(template.FuncMap{
+		"RouteNameToUrl": router.NameToUrl,
+	}).ParseFiles(allFiles...)
+	logger.Error(err)
+
+	// 渲染模板
+	err = tmpl.ExecuteTemplate(w, name, data)
+	if err != nil {
+		return
+	}
+}
+
+func getAdminTemplateFiles(tplFiles ...string) []string {
+	return getTemplateFiles("admin", tplFiles...)
+}
+
+func getTemplateFiles(subViewDir string, tplFiles ...string) []string {
+	// 设置模板相对路径
+	viewDir := "resources/views/" + subViewDir
+
+	// 判断目录是否以 / 字符结尾
+	if !strings.HasSuffix(viewDir, "/") {
+		viewDir = viewDir + "/"
+	}
+
+	// 遍历文件列表 Slice，设置正确的路径，支持 dir.filename 语法糖
+	for i, f := range tplFiles {
+		tplFiles[i] = viewDir + strings.Replace(f, ".", "/", -1) + ".gohtml"
+	}
+
+	// 所有布局模板文件 Slice
+	layoutFiles, err := filepath.Glob(viewDir + "/layouts/*.gohtml")
+	logger.Error(err)
+
+	// 合并所有文件
+	return append(layoutFiles, tplFiles...)
 }
