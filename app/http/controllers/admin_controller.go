1991a7dd18f993faab2656fe857af04585412336
SebastianKennedy <sebastiankennedy@foxmail.com>
2021-11-01 13:29:03 +0800

    Added adminlte html template.

--- app/http/controllers/admin_controller.go
+++ app/http/controllers/admin_controller.go
@@ -1,9 +1,11 @@
 package controllers
 
 import (
+	"github.com/sebastiankennedy/go-web-skeleton/pkg/logger"
 	"github.com/sebastiankennedy/go-web-skeleton/pkg/mvc"
+	"html/template"
 	"net/http"
-	"text/template"
+	"path/filepath"
 )
 
 type AdminController struct {
@@ -11,13 +13,22 @@ type AdminController struct {
 }
 
 func (*AdminController) Index(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("resources/views/admin/layouts/app.gohtml")
-	if err != nil {
-		panic(err)
-	}
+	// 设置模板相对路径
+	viewDir := "resources/views"
 
+	// 所有布局模板文件 Slice
+	files, err := filepath.Glob(viewDir + "/admin/layouts/*.gohtml")
+
+	// 在 Slice 里新增我们的目标文件
+	newFiles := append(files, viewDir+"/admin/dashboard/index.gohtml")
+
+	// 解析模板文件
+	tmpl, err := template.ParseFiles(newFiles...)
+	logger.Error(err)
+
+	// 渲染模板，将数据传输进去
 	err = tmpl.ExecuteTemplate(w, "app", nil)
 	if err != nil {
-		return
+		logger.Error(err)
 	}
 }
