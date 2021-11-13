d960d11b14f013f6203fe08dd602c892efcca1f8
SebastianKennedy <sebastiankennedy@foxmail.com>
2021-11-13 22:20:57 +0800

    Added user register.

--- app/http/controllers/auth_controller.go
+++ app/http/controllers/auth_controller.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"fmt"
+	"github.com/sebastiankennedy/go-web-skeleton/app/http/requests"
 	"github.com/sebastiankennedy/go-web-skeleton/app/models/user"
 	"github.com/sebastiankennedy/go-web-skeleton/pkg/controller"
 	"github.com/sebastiankennedy/go-web-skeleton/pkg/router"
@@ -31,27 +32,36 @@ func (*AuthController) RegisterView(w http.ResponseWriter, r *http.Request) {
 }
 
 func (*AuthController) RegisterOperation(w http.ResponseWriter, r *http.Request) {
-	// 表单验证
-	email := r.PostFormValue("email")
-	username := r.PostFormValue("username")
-	password := r.PostFormValue("password")
-
-	// 验证通过 -- 入库
+	// 获取数据
 	_user := user.User{
-		Username: username,
-		Password: password,
-		Email:    email,
-	}
-	err := _user.Create()
-	if err != nil {
-		return
+		Username:             r.PostFormValue("username"),
+		Email:                r.PostFormValue("email"),
+		Password:             r.PostFormValue("password"),
+		PasswordConfirmation: r.PostFormValue("password_confirmation"),
 	}
 
-	if _user.ID > 0 {
-		fmt.Fprint(w, "注册成功，电子邮箱为 为"+_user.Email)
+	// 表单规则验证数据
+	errs := requests.ValidateRegistrationForm(_user)
+	if len(errs) > 0 {
+		// 表单验证失败，重新显示表单
+		view.RenderSingle(w, view.Data{
+			"Errors": errs,
+			"User":   _user,
+		}, "admin.auth.register")
 	} else {
-		fmt.Fprint(w, "创建用户失败，请联系管理员")
+		err := _user.Create()
+		if err != nil {
+			return
+		}
+
+		if _user.ID > 0 {
+			http.Redirect(w, r, "/", http.StatusFound)
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprint(w, "注册失败，请联系管理员")
+		}
+
 	}
 
-	// 验证失败 -- 重新显示表单
+	// 验证失败,重新显示表单
 }
