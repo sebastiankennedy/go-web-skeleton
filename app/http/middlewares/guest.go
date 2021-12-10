6327ce89ce52dfd64d2bb6e886a4716a69aaef91
SebastianKennedy <sebastiankennedy@foxmail.com>
2021-12-10 13:49:02 +0800

    Added guest middleware.

--- app/http/middlewares/guest.go
+++ app/http/middlewares/guest.go
@@ -1,1 +1,19 @@
 package middlewares
+
+import (
+	"github.com/sebastiankennedy/go-web-skeleton/pkg/auth"
+	"github.com/sebastiankennedy/go-web-skeleton/pkg/flash"
+	"net/http"
+)
+
+func Guest(next http.HandlerFunc) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		if auth.Check() {
+			flash.Warning("登录用户无法访问此页面")
+			http.Redirect(writer, request, "/admin", http.StatusFound)
+			return
+		}
+
+		next(writer, request)
+	}
+}
