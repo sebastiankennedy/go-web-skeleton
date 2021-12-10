bd97c3e8eee186dc730a51a71f06ddd570fa350e
SebastianKennedy <sebastiankennedy@foxmail.com>
2021-12-10 13:48:37 +0800

    Added auth middleware.

---
+++ app/http/middlewares/auth.go
@@ -0,0 +1,19 @@
+package middlewares
+
+import (
+	"github.com/sebastiankennedy/go-web-skeleton/pkg/auth"
+	"github.com/sebastiankennedy/go-web-skeleton/pkg/flash"
+	"net/http"
+)
+
+func Auth(next http.HandlerFunc) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		if !auth.Check() {
+			flash.Warning("登录用户才能访问此页面")
+			http.Redirect(writer, request, "/", http.StatusFound)
+			return
+		}
+
+		next(writer, request)
+	}
+}
